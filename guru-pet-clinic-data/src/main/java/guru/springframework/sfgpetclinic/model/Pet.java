c16f0ee9a3563cadca012944bfb289c040ed6b14
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-15 18:28:31 +0200

    Create Visit  entity

--- guru-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ guru-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -4,31 +4,32 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
 
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthday;
+	private PetType petType;
+	private Owner owner;
+	private LocalDate birthDate;
 
-    public PetType getPetType() {
-        return petType;
-    }
+	public PetType getPetType() {
+		return petType;
+	}
 
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
 
-    public Owner getOwner() {
-        return owner;
-    }
+	public Owner getOwner() {
+		return owner;
+	}
 
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
 
-    public LocalDate getBirthday() {
-        return birthday;
-    }
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
 
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
 }
