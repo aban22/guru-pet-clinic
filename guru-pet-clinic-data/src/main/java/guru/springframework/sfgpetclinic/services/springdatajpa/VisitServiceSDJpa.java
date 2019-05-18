89a4248f0ccbd5b4cb281839196f339e024fa8da
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-18 14:07:02 +0200

    Add VisitServiceSDJpa

---
+++ guru-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitServiceSDJpa.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.respositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+@Service
+@Profile("springdatajpa")
+public class VisitServiceSDJpa implements VisitService {
+
+	private final VisitRepository visitRepository;
+	
+	public VisitServiceSDJpa(VisitRepository visitRepository) {
+		this.visitRepository = visitRepository;
+	}
+	
+	@Override
+	public Set<Visit> findAll() {
+		Set<Visit> visits = new HashSet<>();
+		visitRepository.findAll().forEach(visits::add);
+		return visits;
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return visitRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Visit save(Visit entity) {
+		return visitRepository.save(entity);
+	}
+
+	@Override
+	public void delete(Visit entity) {
+		visitRepository.delete(entity);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		visitRepository.deleteById(id);
+	}
+
+}
--- guru-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ guru-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -10,10 +10,12 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -22,13 +24,15 @@ public class DataLoader implements CommandLineRunner {
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
 	private final SpecialtyService specialtyService;
+	private final VisitService visitService;
 
 	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-			SpecialtyService specialtyService) {
+			SpecialtyService specialtyService, VisitService visitService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
 		this.specialtyService = specialtyService;
+		this.visitService = visitService;
 	}
 
 	@Override
@@ -114,5 +118,14 @@ public class DataLoader implements CommandLineRunner {
 		vetService.save(vet2);
 
 		System.out.println("Loaded Vets....");
+		
+		Visit catVisit = new Visit();
+		catVisit.setPet(fionasPet);
+		catVisit.setDate(LocalDate.now());
+		catVisit.setDescription("Sneezy Kitty");
+		
+		visitService.save(catVisit);
+		
+		System.out.println("Loaded Visits....");
 	}
 }
