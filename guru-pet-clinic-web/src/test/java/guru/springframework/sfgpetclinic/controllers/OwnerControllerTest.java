6a95f0910e6a9e41b1867555810ceb8e658df13e
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-19 22:42:37 +0200

    Display Owner record

--- guru-pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ guru-pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,11 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,9 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.OwnerService;
-
-import static org.hamcrest.Matchers.hasSize;;
+import guru.springframework.sfgpetclinic.services.OwnerService;;
 
 @ExtendWith(MockitoExtension.class)
 public class OwnerControllerTest {
@@ -79,5 +77,18 @@ public class OwnerControllerTest {
 			.andExpect(view().name("notimplemented"));
 		
 		verifyZeroInteractions(ownerService);
+	}
+	
+	@Test
+	public void displayOwner() throws Exception {
+		//given
+		when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+		
+		//when
+		mockMvc.perform(get("/owners/123"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("owners/ownerDetails"))
+			.andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+		//then
 	}
 }
